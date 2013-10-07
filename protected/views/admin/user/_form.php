72811d9053077c6aa11dccdf3927f1f1010b8ef8
Renaud Diez <renaud.diez@sodexo.com>
2013-10-07 07:13:30 +0200

    Add firstname, lastname, homepage and user availability attributes

--- protected/views/admin/user/_form.php
+++ protected/views/admin/user/_form.php
@@ -6,27 +6,39 @@
 
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'user-form',
-	'enableAjaxValidation'=>false,
-)); ?>
+<?php 	$form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+	'id'=>'issue-form',
+	'type'=>'vertical',
+	));
+
+ if($model->isNewRecord)
+ 	$model->setScenario('update');
+ 
+	echo $model->isNewRecord;
+?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
 	<?php echo $form->errorSummary($model); ?>
 	
 	<div class="row">
-		<?php echo $form->labelEx($model,'email'); ?>
-		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>100)); ?>
-		<?php echo $form->error($model,'email'); ?>
+		<?php echo $form->textFieldRow($model,'username',array('size'=>45,'maxlength'=>45)); ?>
 	</div>
-
+	
 	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username',array('size'=>45,'maxlength'=>45)); ?>
-		<?php echo $form->error($model,'username'); ?>
+		<?php echo $form->textFieldRow($model,'email',array('style'=>'width: 400px;','maxlength'=>100)); ?>
 	</div>
 
+	
+	<div class="row">
+		<?php echo $form->textFieldRow($model,'firstname',array('style'=>'width: 300px;','maxlength'=>45)); ?>
+	</div>
+	
+	<div class="row">
+		<?php echo $form->textFieldRow($model,'lastname',array('style'=>'width: 300px;','maxlength'=>45)); ?>
+	</div>
+
+	<?php if($model->isNewRecord):?>
 	<div class="row">
 		<?php echo $form->labelEx($model,'password'); ?>
 		<?php echo $form->passwordField($model,'password',array('size'=>45,'maxlength'=>45)); ?>
@@ -38,8 +50,18 @@
 		<?php echo $form->passwordField($model,'password_repeat',array('size'=>45,'maxlength'=>45)); ?>
 		<?php echo $form->error($model,'password_repeat'); ?>
 	</div>
+	<?php endif;?>
+	
+	<div class="row">
+		<?php echo $form->textFieldRow($model,'hoursbyday',array('style'=>'width: 40px;','maxlength'=>5)); ?>
+	</div>
+	
+	<div class="row">
+		<?php echo $form->textFieldRow($model,'daysbyweek',array('style'=>'width: 40px;','maxlength'=>5)); ?>
+	</div>
+	
 	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+		<?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit', 'type'=>'primary', 'label'=> $model->isNewRecord ? 'Create' : 'Save')); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
